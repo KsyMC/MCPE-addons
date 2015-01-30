65284e9f5758c5eb8ca623395ff7a7aa74c74032
Kim Si Young <ksy4362@naver.com>
2015-01-30 19:26:00 +0900

    Oops.

--- example/ExampleScreen/jni/main.cpp
+++ example/ExampleScreen/jni/main.cpp
@@ -3,8 +3,6 @@
 #include "mcpelauncher.h"
 
 void exampleScreen() {
-	LOGI("제발 뜨라고 ㅣㅁ럼나돋ㅁ환놔ㅣㄴㅇ하;논ㄷㅁㅇㅇ홍혿ㄴㅅㅈ거자");
-
 	// 버튼을 추가하기 위해 init, setupPositions, buttonClicked 세개를 Hook 합니다.
 	mcpelauncher_hook((void *) &Touch::StartMenuScreen::init,					(void *) &Touch::StartMenuScreen::init_hook,					(void **) &Touch::StartMenuScreen::init_real);
 	mcpelauncher_hook((void *) &Touch::StartMenuScreen::setupPositions,	(void *) &Touch::StartMenuScreen::setupPositions_hook,		(void **) &Touch::StartMenuScreen::setupPositions_real);
