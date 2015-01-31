d16295694c54b31c6ed120ca45cca4f0278c7bc2
Hye Seo Lee <hslee@hotmail.de>
2015-01-31 13:26:22 +0900

    Update Android.mk
    그냥 다시 바꿨습니다..

--- example/ExampleScreen/jni/Android.mk
+++ example/ExampleScreen/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 FILE_LIST            := $(wildcard $(LOCAL_PATH)/*.cpp)
 SCREEN_FILE_LIST     := $(wildcard $(LOCAL_PATH)/mcpe/gui/screen/*.cpp)
 
-LOCAL_MODULE    := example.examplescreen
+LOCAL_MODULE    := com.prower.examplescreen
 LOCAL_LDLIBS    := -L$(LOCAL_PATH) -ldl -llog -lEGL -lGLESv2 -lmcpelauncher -lminecraftpe
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_SRC_FILES += $(SCREEN_FILE_LIST:$(LOCAL_PATH)/%=%)
