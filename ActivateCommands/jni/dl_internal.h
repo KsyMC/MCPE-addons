24a8d371f5a17b77762da071f831fb5a89a51996
Kim Si Young <ksy4362@naver.com>
2015-12-31 17:22:47 +0900

    Remove ActivateCommands

--- ActivateCommands/jni/dl_internal.h
+++
@@ -1,8 +0,0 @@
-typedef struct {
-  char name[128];
-  const void* phdr;
-  int phnum;
-  unsigned entry;
-  unsigned base;
-  unsigned size;
-} soinfo2;
--- ActivateCommands/jni/shared.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <android/log.h>
-
-#define LOG_TAG 	"ActivateCommands"
-#define LOGE(...) 	__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) 	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
