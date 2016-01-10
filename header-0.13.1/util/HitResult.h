d913289a8d3f9b166594351cdc9289f75bc05021
Jose Vazquez <jose-vm@users.noreply.github.com>
2016-01-10 01:36:18 -0600

    Update HitResult.h

--- header-0.13.1/util/HitResult.h
+++ header-0.13.1/util/HitResult.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "minecraftpe/block/BlockPos.h"
+#include "minecraftpe/entity/Entity.h"
 #include "minecraftpe/util/Vec3.h"
 
 class HitResult
@@ -10,6 +11,6 @@ public:
 	bool b1;		// 4
 	BlockPos pos;	// 8
 	Vec3 vec3;		// 20
-	int i2;			// 32
+	Entity* e;		// 32
 	bool b2;		// 36
 };
